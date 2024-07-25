40d7dc4a0df3049fc943f01156ea0d67d021e47a
pochemoo <boxofmonster@mail.ru>
2024-07-25 13:43:36 +0500

    add header with menu mobile

---
+++ scripts/menu_mobile.js
@@ -0,0 +1,19 @@
+const openMenuButton = document.querySelector('.header__button-burger');
+const closeMenuButton = document.querySelector('.header__close-button');
+
+const mobileMenu = document.querySelector('.header__menu-mobile');
+
+function openMenuMobile(event) {
+    event.preventDefault();
+
+    mobileMenu.style.width = '100vw';
+}
+
+function closeMenuMobile(event) {
+    event.preventDefault();
+
+    mobileMenu.style.width = '0%';
+}
+
+openMenuButton.addEventListener('click', openMenuMobile.bind(event));
+closeMenuButton.addEventListener('click', closeMenuMobile.bind(event));
